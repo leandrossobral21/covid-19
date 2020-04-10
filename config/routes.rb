4c4066401341d6aed604fbe22c2e18f6b468d322
Gabriela  Toledo Prado <gabriela.prado@youse.com.br>
2020-04-10 16:28:10 -0300

    adicionado gem device e boostrap e criado rota para página principal do site

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'corona/index'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+   root 'corona#index'
 end
