dd0d3ee3bbd8abff813443385812689a2ad32d06
Gabriela  Toledo Prado <gabriela.prado@youse.com.br>
2020-04-10 21:24:11 -0300

    adicionado uma parte do html na página de cadastro

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
-  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password_confirmation, :password, :fullname, :rua, :cep, :bairro, :cidade, :estado, :numero)}
+    
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password_confirmation, :password, :current_password, :fullname, :rua, :cep, :bairro, :cidade, :estado, :numero)}
+  end
 end
