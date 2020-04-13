60614881177ef53c5ea9fb8045040bce01f05266
Gabriela  Toledo Prado <gabriela.prado@youse.com.br>
2020-04-13 19:31:06 -0300

    incluido campos de html no formulario

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password_confirmation, :password, :fullname, :rua, :cep, :bairro, :cidade, :estado, :numero)}
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password_confirmation, :password, :fullname, :rua, :cep, :bairro, :cidade, :estado, :doenca, :fumante)}
     
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password_confirmation, :password, :current_password, :fullname, :rua, :cep, :bairro, :cidade, :estado, :numero)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password_confirmation, :password, :current_password, :fullname, :rua, :cep, :bairro, :cidade, :estado, :doenca, :fumante)}
   end
 end
